8d6d1f1ff5b4a216fff1cc658291336be7f7055a
358878305@qq.com <he358878305>
2016-11-16 15:54:24 +0800

    修改为锁屏界面

--- app/src/main/java/com/clockview/MainActivity.java
+++ app/src/main/java/com/clockview/MainActivity.java
@@ -9,42 +9,48 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.TextView;
+
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
-    MyBroadcastReceiver receiver;
+    private int mYear;
+    private int mMonth;
+    private int mDay;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //隐藏标题栏
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
+
+        initDisplay();
+        initService();
+        initDate();
+    }
+
+    public void initDisplay(){
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED
                 | WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON);
-//        KeyguardManager km = (KeyguardManager) this.getSystemService(Context.KEYGUARD_SERVICE);
-//        km.inKeyguardRestrictedInputMode();
-
-//        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-//            Window window = getWindow();
-//            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS
-//                    | WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
-//            window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-//                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-//                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
-//            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
-//            window.setStatusBarColor(Color.TRANSPARENT);
-//            window.setNavigationBarColor(Color.TRANSPARENT);
-//        }
-
-        //隐藏状态栏
         //定义全屏参数
         int flag=WindowManager.LayoutParams.FLAG_FULLSCREEN;
         //获得当前窗体对象
         Window window=MainActivity.this.getWindow();
         //设置当前窗体为全屏显示
         window.setFlags(flag, flag);
+    }
 
+    public void initService(){
         Intent startIntent = new Intent(this, MyService.class);
         startService(startIntent);
+    }
+
+    public void initDate(){
+        final Calendar c = Calendar.getInstance();
+        mYear = c.get(Calendar.YEAR);
+        mMonth = c.get(Calendar.MONTH);
+        mDay = c.get(Calendar.DAY_OF_MONTH);
+        ((TextView)findViewById(R.id.time)).setText(mYear+"-"+mMonth+"-"+mDay);
     }
 
 }
