4d6de1f44f0232a6fddc90f16fabe80e03bdd3de
358878305@qq.com <he358878305>
2016-11-17 10:48:15 +0800

    屏幕状态监听

--- app/src/main/java/com/clockview/MyBroadcastReceiver.java
+++ app/src/main/java/com/clockview/MyBroadcastReceiver.java
@@ -1,8 +1,10 @@
 package com.clockview;
 
+import android.app.KeyguardManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
 
 /**
  * Created by HJC on  2016/11/10 17:21.
@@ -12,9 +14,13 @@ public class MyBroadcastReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
         if (intent.getAction().equals(Intent.ACTION_SCREEN_OFF)){
-            Intent sayHelloIntent=new Intent(context,MainActivity.class);
-            sayHelloIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-            context.startActivity(sayHelloIntent);
+            KeyguardManager mKeyguardManager = (KeyguardManager) context.getSystemService(Context.KEYGUARD_SERVICE);
+            boolean flag = mKeyguardManager.inKeyguardRestrictedInputMode();
+            if (flag) {
+                Intent sayHelloIntent = new Intent(context, MainActivity.class);
+                sayHelloIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                context.startActivity(sayHelloIntent);
+            }
         }
     }
 }
